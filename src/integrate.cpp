9d0d741bd414436e72b12976d2992891f1ca11f7
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-08 18:07:55 -0400

    moved lorenz63 code into WorkStruct

--- src/integrate.cpp
+++ src/integrate.cpp
@@ -5,8 +5,6 @@
 #include "gsl/gsl_rng.h"
 #include "gsl/gsl_randist.h"
 
-#include "header.h"
-
 using namespace std;
 using namespace arma;
 
@@ -17,32 +15,61 @@ using namespace arma;
 struct WorkStruct {
   const int n;
   vec a, b, c;
-
+  double  sigma, beta, rho, epsilon;
   gsl_rng *rng;
 
-  WorkStruct(int n) : n(n), a(vec(n)), b(vec(n)), c(vec(n)) {
+  // Constructor
+  WorkStruct(int n, double sigma=10.0, double beta=8.0/3.0, double r=28.0,
+	     double epsilon=0.0) :
+    n(n), a(vec(n)), b(vec(n)), c(vec(n)),
+    sigma(10.0), beta(beta), rho(r), epsilon(epsilon) {
     rng = gsl_rng_alloc(gsl_rng_default);
   }
+
+  // Destructor
   ~WorkStruct(){
     free(rng);
   }
 
+  // time stepping routines
   void eulerStep(double t, double dt, vec y);
   void evolve(double tStart, double tEnd, vec& y,
 	      double dtMax);
+
+  // Definition of SDE (can be refactored out)
+  int sdeA(double t,  double y[], double dydt[]) {
+
+    dydt[0] = sigma * (y[1] - y[0]);
+    dydt[1] = y[0] * ( rho - y[2]) - y[1];
+    dydt[2] = y[0] * y[1] - beta * y[2];
+
+    return 0;
+  }
+
+  /* Noise term for  the Lorenz63 system */
+  int sdeB(double t, const double y[], double b[]) {
+
+    int i;
+    for (i = 0; i < n; i++) {
+      b[i] = epsilon;
+    }
+
+    return 0;
+  }
+  
 };
 
 void WorkStruct::eulerStep(double t, double dt, vec y){
-  sdeA(t, y.memptr(), a.memptr(), NULL);
+  sdeA(t, y.memptr(), a.memptr());
 
   // Deterministic predictor corrector
   b = y + dt * a;
-  sdeA(t, b.memptr(), c.memptr(), NULL);
+  sdeA(t, b.memptr(), c.memptr());
   y = y  + dt/2.0 * (a + c);
 
 
   // Stochastic step forward euler
-  sdeB(t, y.memptr(), a.memptr(), NULL);
+  sdeB(t, y.memptr(), a.memptr());
   int i;
   double ys;
   for (i = 0; i < n; i++) {
@@ -96,6 +123,23 @@ int test_eulerStep()
 
 
 int test_evolve()
+{
+   // initialize working arrays
+  const int n =3;
+  WorkStruct work(n);
+  vec y;
+  y << 1 << 1 << 1;
+  double tStart = 0;
+  double tEnd   = 1.0;
+  double dtMax = .01;
+
+  // Evolve function from start to finish
+  work.evolve(tStart, tEnd, y, dtMax);
+
+  return 0;
+}
+
+int test_output()
 {
    // initialize working arrays
   const int n =3;
