4a39f5294e5d7ad0604c2367b340db9b7f743514
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-10 13:51:25 -0400

    command line input

--- src/mcmc.cpp
+++ src/mcmc.cpp
@@ -9,12 +9,13 @@
 
 gsl_rng * r;
 
+double stdev[]= {1.0, .1, 1.0};
+
 using namespace arma;
 using namespace std;
 
 void proposal_rand(vec& X, vec& Y){
   int i;
-  double stdev[]= {.3, .1, 0.3};
   for (i = 0; i < NUMP; i++) {
     Y(i) = X(i) + gsl_ran_gaussian(r, stdev[i]);
   }
@@ -24,7 +25,6 @@ void proposal_rand(vec& X, vec& Y){
 double proposal_pdf(vec& X, vec& Y){
   int i;
   double p = 1.0;
-  double stdev[]= {.5, .1, 1.0};
   for (i = 0; i < NUMP; i++) {
     p *= gsl_ran_gaussian_pdf(Y(i)-X(i), stdev[i]);
   }
--- src/param_search.cpp
+++ src/param_search.cpp
@@ -27,38 +27,48 @@ int test_equil_distr()
   return 0;
 }
 
-int main(int argc, char *argv[])
-{
-  long int N  = 10000;   // Number of samples
-  double noise = 1.0;  // Imperfect model
-  double tau  = 10.0;   // Sampling interval
-  double beta = .04;   // Inverse temperature
-  double tEnd = 100.0; // Sampling length
+void estimate_parameters(long int N, double noise, double beta, double tau, int nt) {
 
-  vec tout = linspace(0.0, tEnd, (int) tEnd /tau);	// Output times
-  vec y("-7.4185 -12.4638 15.6519");			// initial condition
+  // Make a vector of output times at regular intervals set by tau
+  vec tout(nt+1);	
+  int i;
+  for (i = 0; i < nt +1; i++) {
+    tout(i) = i *tau;
+  }
 
-  cout << "Running Truth Model" << endl;
-  auto output = run_model(tout, y);			// get output data
+  vec y("-7.4185 -12.4638 15.6519");		// initial condition
+  auto output = run_model(tout, y);		// get output data
 
   vec theta;					// Parameters
   theta << 10.0 << 8.0/3.0 << 28.0 << noise;	// True values
-  theta(0) = 10.5;				// Make sigma different
+  theta << 13.0 << 5.0/3.0 << 20.0 << noise;	// init cond
 
-  auto f = bind(equil, _1, output, tout, beta);  // equildist for theta f(theta) 
+  auto f = bind(equil, _1, output, tout, beta); // equildist for theta f(theta) 
 
   // Setup output file
   ofstream outfile;
   outfile.open("samples.txt");
-    
+  outfile << "sigma,beta,r,noise" << endl;
+
+  // Run the sampler
   run_mcmc(f, theta, N, [&](vec& x){
       int i;
       for (i = 0; i < x.n_elem; i++) {
 	outfile << x[i];
-	if (i < x.n_elem -1) outfile << " ";
+	if (i < x.n_elem -1) outfile << ",";
       }
       outfile << endl;
     });
   outfile.close();
+}
+
+int main(int argc, char *argv[])
+{
+  long int N = atol(argv[1]);
+  double noise = atof(argv[2]);
+  double beta = atof(argv[3]);
+  double tau = atof(argv[4]);
+  int nt = atol(argv[5]);
+  estimate_parameters(N,  noise,  beta,  tau, nt);
   return 0;
 }
