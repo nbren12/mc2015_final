ea094fe5460afa1741e6f529f9c0a9c5fbdc18e9
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-11 11:22:53 -0400

    added memoization

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import subprocess
 import numpy as np
 import pandas as pd
+import os
+from joblib import Memory
 EXE = 'build/lorenzmcmc'
+jobdir = ".tmpjobs"
 
+if not os.path.exists(jobdir):
+    os.mkdir(jobdir)
 
+memory = Memory(cachedir=jobdir, verbose=0)
+
+@memory.cache
 def run(tau=.01, nt=1000, sigma=10.0, beta=8/3, r=28, noise=0.0):
     args = [EXE, 'run']
     
@@ -13,7 +21,7 @@ def run(tau=.01, nt=1000, sigma=10.0, beta=8/3, r=28, noise=0.0):
     t = np.arange(nt + 1) * tau
     return t, np.genfromtxt("output.txt")
 
-
+@memory.cache
 def sample(N=1000, tau=1.0, nt=10, noise=.1, beta=.01):
     args = [EXE, 'sample']
     floatargs = [tau, nt, noise, beta, N]
